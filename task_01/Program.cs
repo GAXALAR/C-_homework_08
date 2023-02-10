94ab44ddb8e2ea0ef0da67992ecf6a294e2859cb
GAXALAR <ay.kosyrev@gmail.com>
2023-02-10 16:34:43 +0300

    Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.

---
+++ task_01/Program.cs
@@ -0,0 +1,60 @@
+﻿Random rnd = new Random ();
+int m = rnd.Next (4, 10);
+int n = rnd.Next (4, 10);
+int [,] matrix = new int [m,n];
+
+
+
+void PrintArray(int [,] matr)
+    {
+    for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j< matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+  {
+   for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0,10);
+        }
+    } 
+}
+
+
+void Sort (int [,] matr)
+  {
+   for (int i=0; i < matr.GetLength(0); i++)
+    {
+      for (int j=0; j < matr.GetLength(1); j++)
+      {
+        for (int k=0; k < matr.GetLength(1) - 1 ; k++)
+        {
+            if (matr [i , k] <= matr [ i , k + 1 ])
+            {
+                int tmp = matr[i, k + 1];
+                matr[i, k + 1] = matr[i, k ];
+                matr[i, k ] = tmp;
+            }
+           
+        }
+        
+      }
+            
+    } 
+}
+
+FillArray(matrix);
+Console.WriteLine("Массив: ");
+PrintArray (matrix);
+Console.WriteLine();
+Console.WriteLine("Упорядочили строки по убыванию: ");
+Sort(matrix);
+PrintArray(matrix);
